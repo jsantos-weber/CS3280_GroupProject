f805729b5d436cd5aaee228dd515b62f30eb9ff0
colin Pytlik <colinpytlik@mail.weber.edu>
2020-04-18 13:58:11 -0600

    Fnished all except invoice intergration

--- CS3280GP/Main/wndMain.xaml.cs
+++ CS3280GP/Main/wndMain.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CS3280GP.Search;
+using CS3280GP.Items;
+using System.Reflection;
 
 namespace CS3280GP.Main
 {
@@ -20,7 +23,7 @@ namespace CS3280GP.Main
     public partial class wndMain : Window
     {
         Search.wndSearch searchWindow;
-        Items.wndItems searchItems;
+        Items.wndItems ItemsWindow;
         clsMainLogic MainLogic;
         List<clsLineItems> MyList;
         bool editOrAdd;
@@ -28,67 +31,369 @@ namespace CS3280GP.Main
         
         public wndMain()
         {
-            InitializeComponent();
-            searchWindow = new Search.wndSearch(this);
-            searchItems = new Items.wndItems(this);
-            MainLogic = new clsMainLogic();
-            MyList = new List<clsLineItems>();
-            editOrAdd = false;
+            try
+            {
+                InitializeComponent();
+                //searchWindow = new Search.wndSearch(this);
+                //searchItems = new Items.wndItems(this);
+                MainLogic = new clsMainLogic();
+                MyList = new List<clsLineItems>();
+                editOrAdd = false;
 
+                //Items go into combo box
+                Item_List_2.ItemsSource = MainLogic.ListItems();
+
+                //populates data grid
+                Item_Display.ItemsSource = MyList;
+
+                //initalize window
+                ItemsWindow = new Items.wndItems(this);
+
+                Edit_Invoice.IsEnabled = false;
+                Delete_Invoice.IsEnabled = false;
+                Save_Invoice.IsEnabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
         }
 
 
-        private void Add_Item(object sender, RoutedEventArgs e)
+        private void New_invoice(object sender, RoutedEventArgs e)
         {
-            //adds item
+            try
+            {
+                //need invoice intergration
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+            
         }
 
-        private void Delete_Click(object sender, RoutedEventArgs e)
+        private void Save_Invoice_Click(object sender, RoutedEventArgs e)
         {
-            //delete item
+            try
+            {
+                if(Invoice_Date.SelectedDate != null && MyList.Count != 0)
+                {
+                    string date = Invoice_Date.Text;
+                    string tCost = Total_Price.Text;
+
+                    if(editOrAdd)
+                    {
+                        //needs invoice intergration
+                    }
+                    else
+                    {
+                        //needs invoice intergration
+                    }
+                    Edit_Invoice.IsEnabled = true;
+                    Delete_Invoice.IsEnabled = true;
+                    DisableInputs();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        private void Delete_Invoice_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //needs invoice intergration
+
+                MyList.Clear();
+
+                //needs invoice intergration
+
+                UpdateDisplay();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        private void Edit_Invoice_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                editOrAdd = true;
+                Add_Item2.IsEnabled = true;
+                Save_Invoice.IsEnabled = true;
+
+                EnableInputs();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
         }
 
         private void Items_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //detects when the combobox choose an item to update cost
+            try
+            {
+                if(Item_List_2.SelectedIndex != -1)
+                {
+                    var cost = Item_List_2.SelectedValue as clsLineItems;
+                    Price.Text = "$ " + cost.Cost;
+                }
+                else
+                {
+                    Price.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
         }
 
-        private void New_InvoiceClick(object sender, RoutedEventArgs e)
-        {
-            //adds new invoice
-        }
-
-        private void Edit_InvoiceClick(object sender, RoutedEventArgs e)
-        {
-            //edits current invoice
-        }
-
-        private void Search_InvoiceClick(object sender, RoutedEventArgs e)
-        {
-            //opens search window
-            searchWindow = new Search.wndSearch(this);
-            searchWindow.Show();
-            searchWindow.Topmost = true;
-        }
-
-        private void Item_TableClick(object sender, RoutedEventArgs e)
-        {
-            //opens item table window
-            searchItems = new Items.wndItems(this);
-            searchItems.Show();
-            searchItems.Topmost = true;
-        }
-
-        private void OnClose(object sender, EventArgs e)
-        {
-            //what to do when closed
-           
-            
-        }
-
+        /// <summary>
+        /// Only allow numbers to be entered for quantity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Quanitiy_Number_Check(object sender, KeyEventArgs e)
         {
-            //amount of itemsm are numeric only
+            try
+            {
+                //makes it only numbers
+                if(!(e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9 || e.Key >= Key.D0 && e.Key <= Key.D9))
+                {
+                    if(!(e.Key == Key.Back || e.Key == Key.Delete))
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+
         }
+
+        private void Add_item(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(Quanitiy_Input.Text != "" && Item_List_2.SelectedIndex != -1)
+                {
+                    bool ItemListed = false;
+                    var Item = Item_List_2.SelectedValue as clsLineItems;
+
+                    foreach(var listItem in MyList)
+                    {
+                        if(listItem.ItemCode == Item.ItemCode)
+                        {
+                            ItemListed = true;
+                            Int32.TryParse(Price.Text, out int x);
+                            listItem.LineItemNum = x;
+                            break;
+                        }
+                    }
+
+                    if(!ItemListed)
+                    {
+                        Int32.TryParse(Price.Text, out int x);
+                        Item.LineItemNum = x;
+                        MyList.Add(Item);
+                    }
+                    Quanitiy_Input.Text = "";
+
+                    //need to insert invoiuce date
+
+                    UpdateDisplay();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        private void Delete_Item_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var deleteItem = Item_Display.SelectedValue as clsLineItems;
+                MyList.Remove(deleteItem);
+                UpdateDisplay();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Menu control for items
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Item_TableClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                ItemsWindow.ShowDialog();
+                this.Show();
+
+                Quanitiy_Input.Text = "";
+                Price.Text = "";
+                Item_List_2.SelectedIndex = -1;
+                Save_Invoice.IsEnabled = false;
+                DisableInputs();
+                UpdateDisplay();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// menu control for search
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Search_InvoiceClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                searchWindow = new wndSearch(this);
+
+                this.Hide();
+                searchWindow.ShowDialog();
+                this.Show();
+
+                Delete_Invoice.IsEnabled = true;
+                Edit_Invoice.IsEnabled = true;
+                MyList.Clear();
+                Quanitiy_Input.Text = "";
+                Price.Text = "";
+                Item_List_2.SelectedIndex = -1;
+
+                DisableInputs();
+
+                Save_Invoice.IsEnabled = false;
+
+                //will need to pull selected invoice from search window
+
+                UpdateDisplay();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        private void UpdateDisplay()
+        {
+            try
+            {
+                Item_List_2.ItemsSource = MainLogic.ListItems();
+
+                double totalCost = 0;
+
+                // need to get selected invoice, if no invoice, invoice ID: TBD
+
+                foreach(var item in MyList)
+                {
+                    totalCost += (item.Cost * item.LineItemNum);
+                }
+
+                Total_Price.Text = totalCost.ToString();
+
+                Item_Display.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// closes program
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            try
+            {
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// disables input
+        /// </summary>
+        public void DisableInputs()
+        {
+            try
+            {
+                Add_Item2.IsEnabled = false;
+                Item_List_2.IsEnabled = false;
+                Item_Display.IsEnabled = false;
+                Invoice_Date.IsEnabled = false;
+                Quanitiy_Input.IsEnabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// enables inputs
+        /// </summary>
+        public void EnableInputs()
+        {
+            try
+            {
+                Add_Item2.IsEnabled = true;
+                Item_List_2.IsEnabled = true;
+                Item_Display.IsEnabled = true;
+                Invoice_Date.IsEnabled = true;
+                Quanitiy_Input.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Error handler
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + "->" + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
+
+
     }
 }
